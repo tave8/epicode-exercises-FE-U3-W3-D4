ae61a0c8dba654fd24a8ddd57f55aa04580c6e56
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-19 16:33:16 +0100

    finish

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,13 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import favouriteCompaniesReducer from "../reducers/favouriteCompaniesReducer.ts"
 import companiesSearchReducer from "../reducers/companiesSearchReducer.ts"
+// import localStorage from "redux-persist/lib/storage"
 
-const store = configureStore({
-    reducer: {
-        favouriteCompanies: favouriteCompaniesReducer,
-        companiesSearch: companiesSearchReducer
-    }
+// const persistConfig = {
+//   storage: localStorage,
+//   key: "root",
+// }
+
+const bigReducer = combineReducers({
+  favouriteCompanies: favouriteCompaniesReducer,
+  companiesSearch: companiesSearchReducer,
 })
 
+const store = configureStore({
+  reducer: bigReducer,
+})
 
-export default store 
+export default store
+
+
+// WITH REDUX PERSIST - NOT COMPLETED
+// import { combineReducers, configureStore } from "@reduxjs/toolkit"
+// import favouriteCompaniesReducer from "../reducers/favouriteCompaniesReducer.ts"
+// import companiesSearchReducer from "../reducers/companiesSearchReducer.ts"
+// import localStorage from "redux-persist/lib/storage"
+// import { persistReducer, persistStore } from "redux-persist"
+
+// const persistConfig = {
+//   storage: localStorage,
+//   key: "root",
+// }
+
+// const bigReducer = combineReducers({
+//   favouriteCompanies: favouriteCompaniesReducer,
+//   companiesSearch: companiesSearchReducer,
+// })
+
+// const persistedReducer = persistReducer(persistConfig, bigReducer)
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+// })
+
+// const persistedStore = persistStore(store)
+
+// export { store, persistedStore }
